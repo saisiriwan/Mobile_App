cc9595dff297750aaa68c08fee2ab927fa352fea
saisiriwan <siriwanphoksanit22@gmail.com>
2026-03-06 00:06:47 +0700

    Upload calendar_app project

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,24 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_database_web/firebase_database_web.dart';
+import 'package:flutter_native_timezone/flutter_native_timezone_web.dart';
+import 'package:permission_handler_html/permission_handler_html.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  FirebaseFirestoreWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseDatabaseWeb.registerWith(registrar);
+  FlutterNativeTimezonePlugin.registerWith(registrar);
+  WebPermissionHandler.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
